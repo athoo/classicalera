b2a0b7ff08c10a73f7731e168e1d34e33a648e43
athoo <athoo@sina.com>
2015-01-05 00:11:55 +0800

    back finish

--- startbootstrap-bare-1.0.1/js/index.js
+++ startbootstrap-bare-1.0.1/js/index.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
 	var options = { videoId: 'R6OjXXM0paM', start: 3,ratio:4/3 };
-	$('#video').tubular(options);
+	$('.wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
 });
