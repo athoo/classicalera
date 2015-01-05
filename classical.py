8a6044e95481f9a26d84664bd6a3c6c268e43163
athoo <athoo@sina.com>
2015-01-05 21:45:03 +0800

    start route

--- classical.py
+++ classical.py
@@ -1,10 +1,23 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, url_for
 app = Flask(__name__)
 
+namelist=[]
+resultlist=[]
+
 @app.route('/')
-def hello_world():
-    return render_template
+def index():
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+def hello():
+    name = request.form['checking']
+    result = "QQ"+name
+    namelist.append(name)
+    resultlist.append(result)
+    # result = get_result(name)
+    return render_template('index.html', name=name,result=result, namelist=namelist[1:], resultlist=result[1:])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
